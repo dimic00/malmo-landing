a21c68cc7690db16868f68eca3661cea9d892a0c
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-02-26 17:34:42 +0100

    Styling landing page

--- index.php
+++ index.php
@@ -17,10 +17,51 @@
     </head>
 
     <body>
-        <header class="header">
+        <main class="page">
             <div class="container">
-                <img src="library/built/img/malmo-logo.png" alt="Malmö" class="header__img">
+                <header class="header">
+                    <figure class="header__logo">
+                        <img src="library/built/img/malmo-logo.png" alt="Malmö">
+                    </figure>
+                    <nav class="header__menu">
+                        <ul>
+                            <li><a href="#" target="_blank">spotify</a></li>
+                            <li><a href="#" target="_blank">bandcamp</a></li>
+                            <li><a href="#" target="_blank">soundcloud</a></li>
+                            <li><a href="#" target="_blank">youtube</a></li>
+                            <li><a href="#" target="_blank">instagram</a></li>
+                            <li><a href="#" target="_blank">facebook</a></li>
+                            <li><a href="#" target="_blank">merch</a></li>
+                        </ul>
+                    </nav>
+                </header>
+
+                <section class="cntent">
+                    <div class="columns">
+                        <div class="column is-7">
+                            <h3 class="cntent__title">Lost in north.</h3>
+
+                            <div class="cntent__text">
+                                <p>Inspired by the peaceful Scandinavian landscapes, the musical project Malmö takes its name from this famous Swedish city, a place of exchange and dialogue connecting Denmark via the Oresund detroit.</p>
+                                <p>Through his compositions, the Parisian producer creates tactful melodies which are finely cadenced by wooly beats. Ambient, electronica, and felted techno are joined together to give life to evading tracks.</p>
+                            </div>
+                        </div>
+                        <div class="column is-1"></div>
+                        <div class="column is-4">
+                            <div class="cntent__player">
+                                <iframe style="border: 0;" src="https://bandcamp.com/EmbeddedPlayer/album=3495386773/size=large/bgcol=ffffff/linkcol=333333/minimal=true/transparent=true/" seamless></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <footer class="footer">
+                    <div class="footer__link">
+                        contact (booking/other): <a href="mailto:malmo@hej-malmo.se">malmo@hej-malmo.se</a>
+                    </div>
+                </footer>
             </div>
-        </header>
+        </main>
+
     </body>
 </html>
