4091220b17662ec0ed05a6664ca54b37cbeff203
Dimitri <dimitri@macbook-pro-de-dimitri.home>
2020-02-26 15:38:16 +0100

    Head configuration

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Malmö — ambient electronica</title>
+        <meta name="description" content="French ambient electronica producer. Listen, live, contact.">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="UTF-8">
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="320">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="profile" href="http://gmpg.org/xfn/11">
+        <link rel="stylesheet" href="/library/built/style.css">
+
+        <?php if (preg_match('/((http|https):\/\/)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.test\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/', $_SERVER['HTTP_HOST'])) {
+            echo '<script src="//' . $_SERVER['HTTP_HOST'] . ':1337/livereload.js"></script>';
+        } ?>
+    </head>
+
+    <body>
+        
+    </body>
+</html>
